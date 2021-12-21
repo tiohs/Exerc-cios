9b929c35b0744b436976e1db92217b47d63e7e81
tiohs <hamiltonsilva551@gmail.com>
2021-12-21 09:39:44 +0100

    Fibo

--- recursão.pl
+++ recursão.pl
@@ -10,3 +10,23 @@ mdc(X,Y,Resposta):-
 Y>X,
 Yk is Y-X,
 mdc(X,Yk,Resposta).
+
+%Somatorio 
+
+%Caso Base 
+soma_serie(0,0).
+%Indução
+soma_serie(N, Resposta):-
+N>0,
+K is N-1,
+soma_serie(K,Rk),
+Resposta is N+Rk.
+
+% caso base 
+fibo(0,0).
+fibo(1,1).
+fibo(2,1).
+
+%Indução 
+fibo(N,R):-
+N>2
