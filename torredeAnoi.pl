ef1f6269ba9f614b09d10faf01435b74a5a46362
tiohs <hamiltonsilva551@gmail.com>
2022-01-25 10:00:28 +0100

    commit

--- torredeAnoi.pl
+++ torredeAnoi.pl
@@ -1,14 +1,17 @@
 torreHanoi(N):-
-move(N,torreA,torreB,torreC).
+move(N,torreA,torreC,torreB).
 move(0,_,_,_).
 move(N,A,B,C):-
 N1 is N-1,
 move(N1,A,C,B),
-informa(A,B),
+informa(A,B),   
 move(N1,C,B,A).
 informa(A,B):-
 write('Mova o disco da'),
+write('-'),
 write(A),
-write('para a'),
+write(' para a '),
+write('-'),
 write(B),
 nl.
+
