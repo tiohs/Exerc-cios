c6f2a97e797e1c090edd676ef10ccc8422602ad9
tiohs <hamiltonsilva551@gmail.com>
2022-01-26 09:17:59 +0100

    cool

---
+++ classAvalização.pl
@@ -0,0 +1,25 @@
+# clasAvaliacao(A, B, C, D, E).
+# N('18', '19', '20', 'excelente')
+clasAvaliacao(N('18', '19', '20'), 'Excelente').
+clasAvaliacao(N('14','15', '16', '17'), 'Bom').
+clasAvaliacao(N('10', '11', '12', '13'), 'Suficiente').
+clasAvaliacao(N('5', '6', '7', '8', '9'), 'Mediocre').
+clasAvaliacao(N('0', '1', '2', '3', '4'), 'Mau');
+
+clasAvaliacao(N):-
+
+
+
+# clasAvaliacao
+
+# clasAvaliacao(N, clasAvaliacao).
+# N = nota('18', '19', '20').
+# clasAvaliacao = 'Excelente';
+# N = nota('14','15', '16', '17').
+# clasAvaliacao = 'Bom';
+# N = nota('10', '11', '12', '13').
+# clasAvaliacao = 'Suficiente';
+# N = nota('5', '6', '7', '8', '9').
+# clasAvaliacao = 'Mediocre';
+# N = nota('0', '1', '2', '3', '4').
+# clasAvaliacao = 'Mau';
