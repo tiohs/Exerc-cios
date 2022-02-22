b1d0997cf2077c32c4cc0b672c9cae5b98fd725e
tiohs <hamiltonsilva551@gmail.com>
2022-02-22 11:55:54 +0100

    quarta

--- leituraAmigavel.pl
+++ leituraAmigavel.pl
@@ -3,3 +3,21 @@ write('Digite qualquer termo, terminado com um ponto '),
 read(X),
 write('Você escreveu : '),
 write(X).
+
+cubo(X,R):-
+R is X*X*X.
+
+lerTermo:-
+write('Digite o valor de X:'),
+read(X),
+processarTermo(X).
+processarTermo(fim):-!.
+
+processarTermo(X):-
+write('O cubo do valor'),
+write(X),
+write('='),
+cubo(X,R),
+write(R),
+nl,
+lerTermo.
